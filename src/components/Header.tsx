f78e9a862c48e23c4756855ad0663fe8434d19a0
santosh eranki <sai.eranki@healthonus.io>
2024-11-01 15:45:59 +0530

    header chaneg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,36 @@
 import { Nav } from 'react-bootstrap';
-import '../../src/components/Header.css'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import '../../src/components/Header.css';
+
 const Headercomponent = () => {
     const navigate = useNavigate();
+
     const handlelogout = () => {
         localStorage.clear();
         navigate('/');
     }
+
     return (
         <>
             <Nav className='background justify-content-end'>
                 <Nav.Item>
-                    <Nav.Link href="/dashboard" className='text-white'>Home</Nav.Link>
+                    <Nav.Link as={Link} to="/dashboard" className='text-white'>Home</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="link-1" href="/task" className='text-white'>Manage Tasks</Nav.Link>
+                    <Nav.Link as={Link} to="/task" className='text-white'>Manage Tasks</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href="/categories" eventKey="link-1" className='text-white'>Categories</Nav.Link>
+                    <Nav.Link as={Link} to="/categories" className='text-white'>Categories</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href="/accountsettings" eventKey="link-1" className='text-white'>Account</Nav.Link>
+                    <Nav.Link as={Link} to="/accountsettings" className='text-white'>Account</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="link-2" className='text-white' onClick={handlelogout}>Logout</Nav.Link>
+                    <Nav.Link className='text-white' onClick={handlelogout}>Logout</Nav.Link>
                 </Nav.Item>
             </Nav>
         </>
     );
 };
+
 export default Headercomponent;
